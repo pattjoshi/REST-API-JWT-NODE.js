e7073b5819dd4cb8e4895f634d637bc4807d1104
om praksash pattjoshi <78966839+pattjoshi@users.noreply.github.com>
2022-08-12 22:31:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use('/uploads', express.static('uploads'));
 app.use('/', (req, res) => {
     res.send(`
   <h1>Welcome to E-commerce Rest APIs</h1>
-  You may contact me <a href="https://codersgyan.com/links/">here</a>
-  Or You may reach out to me for any question related to this Apis: codersgyan@gmail.com
+  You may contact me <a href="https://github.com/pattjoshi">here</a>
+  Or You may reach out to me for any question related to this Apis: Om prakash Pattjoshi
   `);
 });
 
