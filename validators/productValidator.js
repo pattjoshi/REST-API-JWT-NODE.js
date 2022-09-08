2d46723cc8b7acc24a4730a841aabbd180183f49
pattjoshi <omprakashpattjoshi123@gmail.com>
2022-09-08 22:58:56 +0530

    add

--- validators/productValidator.js
+++ validators/productValidator.js
@@ -1,10 +1,10 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 const productSchema = Joi.object({
-    name: Joi.string().required(),
-    price: Joi.number().required(),
-    size: Joi.string().required(),
-    image: Joi.string(),
+  name: Joi.string().required(),
+  price: Joi.number().required(),
+  size: Joi.string().required(),
+  image: Joi.string(),
 });
 
 export default productSchema;
